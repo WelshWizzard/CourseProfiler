0e610e66b358889041b9bbe40823776f1343a5b8
Shaw Goodwin <shaw.goodwin@gmail.com>
2019-07-13 20:58:27 +0100

    Initial Commit

---
+++ CourseProfiler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseProfiler
+//
+//  Created by Shaw Goodwin on 13/07/2019.
+//  Copyright © 2019 Shaw Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
